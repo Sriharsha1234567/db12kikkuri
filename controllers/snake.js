42919133e3ff099bfa018ee6e5ae2a00e4af5b96
Sriharsha1234567 <sriharshavardhanreddy1234@gmail.com>
2021-11-17 21:30:00 -0600

    added snakedelete file

--- controllers/snake.js
+++ controllers/snake.js
@@ -131,3 +131,16 @@ exports.snake_update_Page =  async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 };  
+// Handle a delete one view with id from query 
+exports.snake_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await snake.findById(req.query.id) 
+        res.render('snakedelete', { title: 'snake Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/snake.js
+++ routes/snake.js
@@ -9,5 +9,7 @@ router.get('/detail', snake_controlers.snake_view_one_Page);
 /* GET create snake page */ 
 router.get('/create', snake_controlers.snake_create_Page);
 /* GET create update page */ 
-router.get('/update', snake_controlers.snake_update_Page);  
+router.get('/update', snake_controlers.snake_update_Page); 
+/* GET create snake page */ 
+router.get('/delete', snake_controlers.snake_delete_Page); 
 module.exports = router; 
